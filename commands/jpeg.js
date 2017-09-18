7f14fbf3580cfdb38fc3ff26c0749e8eaa9b7236
Hansen <hansenhasdepression@gmail.com>
2017-09-17 20:31:56 -0400

    optimized jpeg

--- commands/jpeg.js
+++ commands/jpeg.js
@@ -1,52 +1,31 @@
 var Jimp = require("jimp")
-var snekfetch = require("snekfetch")
-var fs = require("fs")
-exports.run = (bot, msg, args) =>{
-if(!args.startsWith("http")){
+exports.run = (bot, msg, args) => {
+	let url = args
 	var user = msg.mentions.users.first();
-	if(!user){
-Jimp.read(msg.attachments.first().url, function (err, image) {
-    if(err){ msg.reply("I FOUND A ERR "+ err)
-           return
-           }else
-        image.quality(1)
-	           .quality(1)
-             .write("./jpeg.jpg")
-  msg.channel.send({
-			files: [{
-				attachment: "./jpeg.jpg",
-				name: 'jpeg.jpg'
-			}]
-		});
-})
-	}else
-Jimp.read(user.avatarURL, function (err, image) {
-    if(err){ msg.reply("I FOUND A ERR "+ err)
-           return
-           }else
-        image.quality(1)
-	           .quality(1)
-             .write("./jpeg.jpg")
-  msg.channel.send({
-			files: [{
-				attachment: "./jpeg.jpg",
-				name: 'jpeg.jpg'
-			}]
-		});
-})
-}else
-	Jimp.read(args, function (err, image) {
-    if(err){ msg.reply("I FOUND A ERR "+ err)
-           return
-           }else
-        image.quality(1)
-		         .quality(1)
-             .write("./jpeg.jpg")
-  msg.channel.send({
-			files: [{
-				attachment: "./jpeg.jpg",
-				name: 'jpeg.jpg'
-			}]
-		});
-})
+	function jpeg(url) {
+		Jimp.read(url, function(err, image) {
+			if (err) {
+				msg.reply("I FOUND A ERR " + err)
+				return
+			} else
+				image.quality(1)
+				.quality(1)
+				.write("./jpeg.jpg")
+			msg.channel.send({
+				files: [{
+					attachment: "./jpeg.jpg",
+					name: 'jpeg.jpg'
+				}]
+			});
+		})
+	}
+	if (!url.startsWith("http")) {
+		if (!user) {
+			url = msg.attachments.first().url
+			jpeg(url)
+		} else
+			url = user.avatarURL
+		jpeg(url)
+	} else
+		jpeg(url)
 }
