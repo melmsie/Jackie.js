a8c25bf3130fdc7d5c7aafe6953a87cb7e5dc682
Hansen <hansenhasdepression@gmail.com>
2017-09-20 20:47:45 -0400

    bepsi

--- commands/mm.js
+++ commands/mm.js
@@ -1,5 +1,7 @@
 let config = require("../config.json")
 exports.run = (bot, msg) => {
-let henlo = Math.floor(Math.random() * config.mm.length)
+let henlo = Math.floor(Math.random() * config.mm.length / 2 + 1)
+let bepsi = Math.floor(Math.random() * config.mm.length / 2 - 1)
+    henlo = bepsi + henlo
 msg.channel.send(config.mm[henlo])
 }
