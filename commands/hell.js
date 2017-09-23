1bcd930463c61ba47886a4ed8abad1c5307a6c2b
Hansen <hansenhasdepression@gmail.com>
2017-09-23 17:15:24 -0400

    WHAT THE HELL AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ commands/hell.js
@@ -0,0 +1,30 @@
+let jimp = require("jimp")
+exports.run = (bot, msg , args) =>{
+  let url = args
+	let user = msg.mentions.users.first();
+	function hell(url) {
+	jimp.read("https://www.api.jackiejs.xyz/imgen/hell.png", function (err, image) {
+    jimp.read(url, function (err, img) {
+       img.resize(500 , 429)
+      image.composite(img , 0 , 0)
+           .write("./hell.jpg")
+           msg.channel.send({
+             files: [{
+               attachment: "./hell.jpg",
+               name: 'oh_god.jpg'
+             }]
+           });
+  })
+})
+		}
+
+	if (!url.startsWith("http")) {
+		if (!user) {
+			url = msg.attachments.first().url
+			hell(url)
+		} else
+			url = user.avatarURL
+		hell(url)
+	} else
+		hell(url)
+}
