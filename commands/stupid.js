f1f32f59178fa95950531081232ad4d193f1bd3b
Hansen <hansenhasdepression@gmail.com>
2017-09-22 20:48:23 -0400

    ur stupid STUPID ASS

---
+++ commands/stupid.js
@@ -0,0 +1,28 @@
+let jimp = require("jimp")
+exports.run = (bot, msg , args) =>{
+  let url = args
+	let user = msg.mentions.users.first();
+	function stupuid(url) {
+	let img =	Jimp.read("https://www.api.jackiejs.xyz/imgen/stupid.png")
+  let img2 = jimp.read(url)
+  img2.resize(150,150)
+  img.composite(img2, 10 , 90);
+  img.write("./stupid.jpg")
+      msg.channel.send({
+				files: [{
+					attachment: "./stupid.jpg",
+					name: 'stupid.jpg'
+				}]
+			});
+		}
+	
+	if (!url.startsWith("http")) {
+		if (!user) {
+			url = msg.attachments.first().url
+			stupuid(url)
+		} else
+			url = user.avatarURL
+		stupuid(url)
+	} else
+		stupuid(url)
+}
