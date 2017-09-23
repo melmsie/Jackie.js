7ef2ce4265114d8995fe28b64e56137a74edb44f
Hansen <hansenhasdepression@gmail.com>
2017-09-22 20:57:59 -0400

    stupider LUL

--- commands/stupid.js
+++ commands/stupid.js
@@ -3,7 +3,7 @@ exports.run = (bot, msg , args) =>{
   let url = args
 	let user = msg.mentions.users.first();
 	function stupuid(url) {
-	let img =	Jimp.read("https://www.api.jackiejs.xyz/imgen/stupid.png")
+	let img =	jimp.read("https://www.api.jackiejs.xyz/imgen/stupid.png")
   let img2 = jimp.read(url)
   img2.resize(150,150)
   img.composite(img2, 10 , 90);
@@ -15,7 +15,7 @@ exports.run = (bot, msg , args) =>{
 				}]
 			});
 		}
-	
+
 	if (!url.startsWith("http")) {
 		if (!user) {
 			url = msg.attachments.first().url
