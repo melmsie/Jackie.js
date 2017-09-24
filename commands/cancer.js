39dd08d1865e58a89c4081e24c35aafa54fed749
Hansen <hansenhasdepression@gmail.com>
2017-09-24 14:58:27 -0400

    aeth and melms didn't take my template for dank memer so sucks for them

---
+++ commands/cancer.js
@@ -0,0 +1,30 @@
+let jimp = require("jimp")
+exports.run = (bot, msg , args) =>{
+  let url = args
+	let user = msg.mentions.users.first();
+	function cancer(url) {
+	jimp.read("https://www.api.jackiejs.xyz/imgen/cancer.png", function (err, image) {
+    jimp.read(url, function (err, img) {
+       img.resize(320 , 377)
+      image.composite(img , 320 , 192)
+           .write("./cancer.jpg")
+           msg.channel.send({
+             files: [{
+               attachment: "./cancer.jpg",
+               name: 'cancer.jpg'
+             }]
+           });
+  })
+})
+		}
+
+	if (!url.startsWith("http")) {
+		if (!user) {
+			url = msg.attachments.first().url
+			cancer(url)
+		} else
+			url = user.avatarURL
+		cancer(url)
+	} else
+		cancer(url)
+}
