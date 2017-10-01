b077ed902745682cb60633807722e67b0cf24971
Austin Melms <memyselfandlie@gmail.com>
2017-10-01 13:14:30 -0400

    smol cleanup until I have more time

--- Jackie.js
+++ Jackie.js
@@ -24,68 +24,73 @@ metrics.init({ host: 'myhost', prefix: 'jj.',apiKey: config.keys.ddkey, });
   \___/ \__,_|\___|_|\_\_|\___|
 
 Big Help: August!
+No Help Melmsie
 Thanks to august for command handler and logger. <3
+Thanks to Melmsie for nothing
 */
 
 function serverCount() {
-  client.shard.fetchClientValues("guilds.size").then(result => {
-  const guildsizes = result.reduce((prev, val) => prev + val, 0)
-      snekfetch.post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
-          .set("Authorization", oliyBots)
-          .send({"server_count": guildsizes})
-          .then(console.log("[oliyBots] Post Stats!"))
-  snekfetch.post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`)
-      .set("Authorization", dBots)
-      .send({"server_count": client.guilds.size})
-      .then(console.log("[dBots] Post Stats!"))
-   snekfetch.post(`https://novo.archbox.pro/api/bots/${client.user.id}`)
-		        .set("Authorization", msbots)
-		        .send({"server_count":guildsizes})
-	          .then(console.log("[mbots] Post Stats!"));
- 	metrics.gauge('guilds', guildsizes);
+  	client.shard.fetchClientValues("guilds.size").then(result => {
+		const guildsizes = result.reduce((prev, val) => prev + val, 0)
+		snekfetch.post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
+        		.set("Authorization", oliyBots)
+        		.send({"server_count": guildsizes})
+        		.then(console.log("[oliyBots] Post Stats!"))
+		snekfetch.post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`)
+			.set("Authorization", dBots)
+			.send({"server_count": client.guilds.size})
+			.then(console.log("[dBots] Post Stats!"))
+		snekfetch.post(`https://novo.archbox.pro/api/bots/${client.user.id}`)
+			.set("Authorization", msbots)
+			.send({"server_count":guildsizes})
+			.then(console.log("[mbots] Post Stats!"));
+		metrics.gauge('guilds', guildsizes);
 		console.log(guildsizes)
 	})
 }
-function setgame(){
+
+function setgame() 
 	let gamesnum = Math.floor(Math.random() * games.length)
-bot.user.setPresence({ game: { name: games[gamesnum] + " |+-help", type: 0 } });
+	bot.user.setPresence({ game: { name: games[gamesnum] + " |+-help", type: 0 } });
 	}
+
 bot.on('ready', () => {
-  serverCount();
+  	serverCount();
 	setgame();
-  bot.channels.get(config.logger.shardchannel).send(`I'm Ready!`)
+	bot.channels.get(config.logger.shardchannel).send(`I'm Ready!`)
 })
 
 bot.on('guildCreate', (guild) =>{
 	setgame();
 	serverCount();
-  bot.channels.get(config.logger.createchannel).send(`Joined a new server! Guild Name: ${guild.name}`)
+	bot.channels.get(config.logger.createchannel).send(`Joined a new server! Guild Name: ${guild.name}`)
 });
 
 bot.on('guildDelete', (guild) =>{
-		setgame();
+	setgame();
 	serverCount();
-  bot.channels.get(config.logger.deletechannel).send(`Lefted a server! Guild Name: ${guild.name}`)
+	bot.channels.get(config.logger.deletechannel).send(`Lefted a server! Guild Name: ${guild.name}`)
 });
 
 bot.on('message', msg => {
 	metrics.increment('messagesgot');
 	if (msg.author.bot || !msg.content.startsWith(prefix)){
 		return;
-	} else
-	if(bot.user.id !== "327135412806221826" || prefix !== "+-" || config.ownerID !== "214382760826109953"){
+	}
+	if (bot.user.id !== "327135412806221826" || prefix !== "+-" || config.ownerID !== "214382760826109953"){
 		return msg.reply("This is a stolen bot.");
-	}else
-		metrics.increment('commandsusage');
+	}
+	metrics.increment('commandsusage');
 	setgame();
-  var user = msg.mentions.users.first();
-  const args = msg.content.split(" ").slice(1).join(" ")
-  const command = msg.content.split(" ").shift().slice(prefix.length)
-  try {
-     let commandFile = require(`./commands/${command}.js`)
-    commandFile.run(bot, msg, args, user)
-    } catch (err) {
-      logger.warn(err);
-   }
+  	const user = msg.mentions.users.first();
+  	const args = msg.content.split(" ").slice(1).join(" ")
+  	const command = msg.content.split(" ").shift().slice(prefix.length)
+  	try {
+   	  let commandFile = require(`./commands/${command}.js`)
+  	  commandFile.run(bot, msg, args, user)
+  	  } catch (err) {
+  	    logger.warn(err.stack);
+  	 }
 });
+
 bot.login(config.keys.token);
